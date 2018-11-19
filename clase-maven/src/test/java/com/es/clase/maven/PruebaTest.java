fbb120bbd898b81adfd463b1a89889dcf73c88a4
ivo-koch <ikoch@ungs.edu.ar>
2018-11-19 13:22:23 -0300

    inicial

---
+++ clase-maven/src/test/java/com/es/clase/maven/PruebaTest.java
@@ -0,0 +1,14 @@
+package com.es.clase.maven;
+
+import junit.framework.TestCase;
+
+public class PruebaTest extends TestCase {
+	
+
+    public void testMetodo1()
+    {
+        assertTrue( true );
+    }
+	
+
+}
